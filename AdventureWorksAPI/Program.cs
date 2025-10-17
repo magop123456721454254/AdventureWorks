0f4f3de2921b9b738803e7943f99ed704e7e0587
magnussunesop <magnus.sunesson@omegapoint.se>
2025-10-17 14:50:32 +0200

    add support for swagger to api

--- AdventureWorksAPI/Program.cs
+++ AdventureWorksAPI/Program.cs
@@ -1,6 +1,7 @@
 ﻿using AdventureWorksAPI.Services;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,12 +14,23 @@ builder.Services.AddOpenApi();
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 
+builder.Services.AddEndpointsApiExplorer();  // for Swagger
+builder.Services.AddSwaggerGen();            // for Swagger UI
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "AdventureWorks API", Version = "v1" });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "AdventureWorks API V1"));
 }
 
 app.MapControllers();
