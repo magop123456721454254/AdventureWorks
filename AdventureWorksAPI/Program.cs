f2fd6af44e14a2be59b7fe3846217986f4b6324d
magnussunesop <magnus.sunesson@omegapoint.se>
2025-10-20 09:44:54 +0200

    CORS policy fix

--- AdventureWorksAPI/Program.cs
+++ AdventureWorksAPI/Program.cs
@@ -5,17 +5,13 @@ using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContextFactory<AdventureWorksContext>(options => 
+builder.Services.AddDbContextFactory<AdventureWorksContext>(options =>
     options.UseSqlServer(
         "Server=localhost\\SQLEXPRESS;Database=AdventureWorks2016;User Id=sa;Password=YourStrong!Passw0rd;TrustServerCertificate=True;"));
 
 builder.Services.AddScoped<PersonService>();
 builder.Services.AddOpenApi();
 builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-
-builder.Services.AddEndpointsApiExplorer();  // for Swagger
-builder.Services.AddSwaggerGen();            // for Swagger UI
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
@@ -23,7 +19,20 @@ builder.Services.AddSwaggerGen(c =>
     c.SwaggerDoc("v1", new OpenApiInfo { Title = "AdventureWorks API", Version = "v1" });
 });
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("MyNextJSCORS",
+        policy =>
+        {
+            policy
+                .WithOrigins("http://localhost:3000")
+                .AllowAnyHeader()
+                .AllowAnyMethod();
+        });
+});
+
 var app = builder.Build();
+app.UseCors("MyNextJSCORS");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -32,6 +41,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "AdventureWorks API V1"));
 }
+
+app.UseAuthorization();
+
 
 app.MapControllers();
 
