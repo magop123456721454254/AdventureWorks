eff1d5b8314225e8ec2c4bf241b79efa3995e416
magnussunesop <magnus.sunesson@omegapoint.se>
2025-10-24 11:40:29 +0200

    Check that service returns Ok

--- AdventureWorksAPI/Controllers/PersonController.cs
+++ AdventureWorksAPI/Controllers/PersonController.cs
@@ -3,9 +3,6 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace AdventureWorksAPI.Controllers
 {
-
-    // TEST COMMENT FOR FIRST PR
-
     [ApiController]
     [Route("api/[controller]")]
     public class PersonController : ControllerBase
--- AdventureWorksAPITest/PersonControllerTest.cs
+++ AdventureWorksAPITest/PersonControllerTest.cs
@@ -8,27 +8,28 @@ namespace AdventureWorksAPITest
     public class PersonControllerTest
     {
         [Fact]
-        public void GetPersons_ReturnsOk_WithPersonList()
+        public void GetPersons_ReturnsOkResult()
         {
             // Arrange
-            var mockService = new Mock<PersonService>();
-            mockService
-                .Setup(s => s.GetPersons(2))
-                .Returns(new List<Person>
-                {
-                    new Person { FirstName = "Alice", LastName = "Smith" },
-                    new Person { FirstName = "Bob", LastName = "Lazar" }
-                });
-
+            var mockService = new Mock<MockedPersonService>();
             var controller = new PersonController(mockService.Object);
 
             // Act
             var results = controller.GetPersons();
 
             // Assert
-            var okResult = Assert.IsType<ObjectResult>(results);
-            var persons = Assert.IsAssignableFrom<IEnumerable<Person>>(okResult.Value);
-            Assert.Equal(2, persons.Count());
+
+            var okRes = Assert.IsType<OkObjectResult>(results);
+
+            //var list = Assert.IsType<IActionResult>(results, exactMatch: false);
+            //bool ok = results.ExecuteResultAsync();
+
+            //var person = list.First();
+            //Assert.Equal("Alice", person.FirstName);
+
+            //var okResult = Assert.IsType<ObjectResult>(results);
+            //var persons = Assert.IsAssignableFrom<IEnumerable<Person>>(okResult.Value);
+            //Assert.Equal(2, persons.Count());
         }
     }
 }
