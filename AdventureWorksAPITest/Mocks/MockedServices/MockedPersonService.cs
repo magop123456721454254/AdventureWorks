b3b41f4a5fe844b6f9712f16bca09ccf1e00568e
magnussunesop <magnus.sunesson@omegapoint.se>
2025-10-24 11:39:20 +0200

    Add mock for person service

---
+++ AdventureWorksAPITest/Mocks/MockedServices/MockedPersonService.cs
@@ -0,0 +1,28 @@
+﻿using AdventureWorksAPI.Services;
+
+namespace AdventureWorksAPITest
+{
+    public class MockedPersonService : IPersonService
+    {
+        public MockedPersonService() { }
+
+        public virtual IEnumerable<Person> GetPersons()
+        {
+            return new List<Person>
+                {
+                    new Person { FirstName = "Alice", LastName = "Smith" },
+                    new Person { FirstName = "Bob", LastName = "Lazar" }
+                };
+        }
+
+        public virtual IEnumerable<Person> GetPersons(int amount)
+        {
+            return new List<Person>
+                {
+                    new Person { FirstName = "Alice", LastName = "Smith" },
+                    new Person { FirstName = "Bob", LastName = "Lazar" },
+                    new Person { FirstName = "Stan", LastName = "Carley" }
+                };
+        }
+    }
+}
