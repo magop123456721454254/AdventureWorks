2d12fa52e1610b8c622a8b3044bca7b943080dcb
magnussunesop <magnus.sunesson@omegapoint.se>
2025-10-28 11:08:35 +0100

    Add Dto model

---
+++ AdventureWorksAPI/Models/PersonDto.cs
@@ -0,0 +1,15 @@
+﻿namespace AdventureWorksAPI.Models
+{
+    public class PersonDto
+    {
+        public int? BusinessEntityId { get; set; }
+        public string? PersonType { get; set; }
+        public string? Title { get; set; }
+        public string? FirstName { get; set; }
+        public string? MiddleName { get; set; }
+        public string? LastName { get; set; }
+        public string? Suffix { get; set; }
+        public int? EmailPromotion { get; set; }
+        public bool? IsActive { get; set; }  
+    }
+}
