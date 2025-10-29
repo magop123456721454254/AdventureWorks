844754bc9e9072d286b368a138461ca18022a322
magnussunesop <magnus.sunesson@omegapoint.se>
2025-10-29 14:18:29 +0100

    Add string lengths in model to handle erronous input on application level instead of db

--- AdventureWorksAPI/Models/PersonDto.cs
+++ AdventureWorksAPI/Models/PersonDto.cs
@@ -1,14 +1,29 @@
-﻿namespace AdventureWorksAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AdventureWorksAPI.Models
 {
     public class PersonDto
     {
+        [StringLength(2)]
         public string? PersonType { get; set; }
+        
+        [StringLength(50)]
         public string? Title { get; set; }
+        
+        [StringLength(50)]
         public string? FirstName { get; set; }
+        
+        [StringLength(50)]
         public string? MiddleName { get; set; }
+        
+        [StringLength(50)]
         public string? LastName { get; set; }
+        
+        [StringLength(50)]
         public string? Suffix { get; set; }
+
         public int? EmailPromotion { get; set; }
-        public bool? IsActive { get; set; }  
+
+        public bool? IsActive { get; set; }
     }
 }
