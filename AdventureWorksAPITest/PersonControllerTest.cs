73d4adcb353a8f7a09121b453956dfd1dd4fd33f
magnussunesop <magnus.sunesson@omegapoint.se>
2025-10-30 11:40:05 +0100

    Rewrite MoQ

--- AdventureWorksAPITest/PersonControllerTest.cs
+++ AdventureWorksAPITest/PersonControllerTest.cs
@@ -13,31 +13,21 @@ namespace AdventureWorksAPITest
         public void GetPersons_ReturnsOkResult()
         {
             // Arrange
-            var mockService = new Mock<MockedPersonService>();
+            var mockService = new Mock<IPersonService>();
+            mockService.Setup(s => s.GetPerson(50)).Returns(new Person { FirstName = "Alice", BusinessEntityId = 50 });
             var controller = new PersonController(mockService.Object);
-
             // Act
             var results = controller.GetPersonsList();
 
             // Assert
             var okRes = Assert.IsType<OkObjectResult>(results);
-
-            //var list = Assert.IsType<IActionResult>(results, exactMatch: false);
-            //bool ok = results.ExecuteResultAsync();
-
-            //var person = list.First();
-            //Assert.Equal("Alice", person.FirstName);
-
-            //var okResult = Assert.IsType<ObjectResult>(results);
-            //var persons = Assert.IsAssignableFrom<IEnumerable<Person>>(okResult.Value);
-            //Assert.Equal(2, persons.Count());
         }
 
         [Fact]
         public void GetPersons_WithParameter_ReturnsOkResult()
         {
             // Arrange
-            var mockService = new Mock<MockedPersonService>();
+            var mockService = new Mock<IPersonService>();
             var controller = new PersonController(mockService.Object);
 
             // Act
@@ -51,34 +41,53 @@ namespace AdventureWorksAPITest
         public void GetPerson_WithSpecificId_ReturnsOk()
         {
             // Arrange
-            var mockService = new Mock<MockedPersonService>();
+            var mockService = new Mock<IPersonService>();
             var controller = new PersonController(mockService.Object);
 
             // Act
             var results = controller.GetPerson(50);
 
             // Assert
-            var okRes = Assert.IsType<OkObjectResult>(results);
+            Assert.IsType<OkObjectResult>(results);
         }
+
+        [Fact]
+        public void GetPerson_WithBadId_ReturnsNotFound()
+        {
+            // Arrange
+            var mockService = new Mock<IPersonService>();
+            mockService.Setup(s => s.GetPerson(50)).Returns(new Person { FirstName = "Alice", BusinessEntityId = 50 });
+            var controller = new PersonController(mockService.Object);
+
+            // Act
+            var results = controller.GetPerson(51);
+
+            // Assert
+            Assert.IsType<NotFoundResult>(results);
+        }
+
+
 
         [Fact]
         public void GetPerson_WithSpecificId_HasCorrectValues()
         {
             // Arrange
-            var mockService = new Mock<MockedPersonService>();
-            var controller = new PersonController(mockService.Object);
+            var mockService = new Mock<IPersonService>();
+            mockService.Setup(s => s.GetPerson(50)).
+                Returns(
+                new Person
+                {
+                    BusinessEntityId = 50,
+                    PersonType = "EM",
+                    Title = null,
+                    FirstName = "Alice",
+                    MiddleName = "M",
+                    LastName = "Higa",
+                    Suffix = null,
+                    EmailPromotion = 0
+                });
 
-            Person matchPerson = new Person
-            {
-                BusinessEntityId = 50,
-                PersonType = "EM",
-                Title = null,
-                FirstName = "Sidney",
-                MiddleName = "M",
-                LastName = "Higa",
-                Suffix = null,
-                EmailPromotion = 0
-            };
+            var controller = new PersonController(mockService.Object);
 
             // Act
             var result = controller.GetPerson(50);
@@ -87,12 +96,10 @@ namespace AdventureWorksAPITest
             var okResult = Assert.IsType<OkObjectResult>(result);
             var person = Assert.IsType<Person>(okResult.Value);
 
-            Assert.Equal(person.FirstName, matchPerson.FirstName);
-            Assert.Equal(person.LastName, matchPerson.LastName);
-            Assert.Equal(person.MiddleName, matchPerson.MiddleName);
-            Assert.Equal(person.Title, matchPerson.Title);
-            Assert.Equal(person.PersonType, matchPerson.PersonType);
-
+            Assert.Equal("Alice", person.FirstName);
+            Assert.Equal("Higa", person.LastName);
+            Assert.Equal("M", person.MiddleName);
+            Assert.Equal("EM", person.PersonType);
         }
 
         [Fact]
@@ -117,6 +124,7 @@ namespace AdventureWorksAPITest
         [Fact]
         public void SoftDeletePerson_Should_Set_IsActive_False()
         {
+            // Arrange
             var options = new DbContextOptionsBuilder<AdventureWorksContext>()
                 .UseInMemoryDatabase(databaseName: "SoftDeleteTestDb")
                 .Options;
@@ -145,6 +153,7 @@ namespace AdventureWorksAPITest
         [Fact]
         public void ReactivatePerson_ReactivatesPerson()
         {
+            // Arrange
             var options = new DbContextOptionsBuilder<AdventureWorksContext>()
                 .UseInMemoryDatabase(databaseName: "SoftDeleteTestDb")
                 .Options;
