08178a86a675f8b6f81962184ffa26a473b183ec
magnussunesop <magnus.sunesson@omegapoint.se>
2025-11-05 15:49:02 +0100

    Fix some infrastructure for apollo to work

--- adventureworks-ui/src/app/page.tsx
+++ adventureworks-ui/src/app/page.tsx
@@ -1,53 +1,27 @@
-// src/app/products/page.tsx
 'use client';
-
-import { useEffect, useState } from 'react';
-import { Person } from './models/personModel';
-import TabsComponent from './components/tabs/tabs-component';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
+import { useQuery } from '@apollo/client';
+import { GET_PERSONS } from './functions/queries/personQueries';
+import { Person } from '@/app/models/personModel';
 
 export default function PersonsPage() {
-  const [persons, setPersons] = useState<Person[]>([]);
-  const apiUrl = process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000";
-  fetch(`${apiUrl}/api/Person/GetPersonsList/`);
-
-  const darkTheme = createTheme({
-    palette: {
-      mode: 'dark',
-    },
+  const { data, loading, error, fetchMore } = useQuery(GET_PERSONS, {
+    variables: { first: 5, after: null },
   });
 
-  useEffect(() => {
-    fetch(`${apiUrl}/api/Person/GetPersonsList/20`)
-      .then((res) => res.json())
-      .then((data) => setPersons(data));
-  }, [apiUrl]);
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
 
   return (
-    <div className="p-3 content-center">
-      <table className="w-3/4 border border-gray-200">
-        <thead className="bg-sky-600">
-          <tr>
-            <th className="py-2 px-4 text-left border-b">ID</th>
-            <th className="py-2 px-4 text-left border-b">Title</th>
-            <th className="py-2 px-4 text-left border-b">First Nssssame</th>
-            <th className="py-2 px-4 text-left border-b">Middle Name</th>
-            <th className="py-2 px-4 text-left border-b">Last Name</th>
-          </tr>
-        </thead>
-        <tbody>
-          {persons.map((p) => (
-            <tr key={p.businessEntityId} className="hover:bg-sky-800">
-              <td className="py-2 px-4 border-b">{p.businessEntityId}</td>
-              <td className="py-2 px-4 border-b">{p.title}</td>
-              <td className="py-2 px-4 border-b">{p.firstName}</td>
-              <td className="py-2 px-4 border-b">{p.middleName}</td>
-              <td className="py-2 px-4 border-b">{p.lastName}</td>
-            </tr>
+    <html>
+      <body>
+        <ul>
+          {data.personsEndpoint.nodes.map((person: Person) => (
+            <li key={person.businessEntityId}>
+              {person.firstName} {person.lastName}
+            </li>
           ))}
-        </tbody>
-      </table>
-    </div>
+        </ul>
+      </body>
+    </html>
   );
 }
