a2921e8d850f9670a276371d10361699bd201b9f
magnussunesop <magnus.sunesson@omegapoint.se>
2025-11-05 12:46:15 +0100

    Casing fix

--- adventureworks-ui/src/app/page.tsx
+++ adventureworks-ui/src/app/page.tsx
@@ -2,8 +2,8 @@
 'use client';
 
 import { useEffect, useState } from 'react';
-import { Person } from './Models/PersonModel';
-import TabsComponent from './Components/Tabs/tabs-component';
+import { Person } from './models/personModel';
+import TabsComponent from './components/tabs/tabs-component';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 
@@ -26,28 +26,28 @@ export default function PersonsPage() {
 
   return (
     <div className="p-3 content-center">
-        <table className="w-3/4 border border-gray-200">
-          <thead className="bg-sky-600">
-            <tr>
-              <th className="py-2 px-4 text-left border-b">ID</th>
-              <th className="py-2 px-4 text-left border-b">Title</th>
-              <th className="py-2 px-4 text-left border-b">First Nssssame</th>
-              <th className="py-2 px-4 text-left border-b">Middle Name</th>
-              <th className="py-2 px-4 text-left border-b">Last Name</th>
+      <table className="w-3/4 border border-gray-200">
+        <thead className="bg-sky-600">
+          <tr>
+            <th className="py-2 px-4 text-left border-b">ID</th>
+            <th className="py-2 px-4 text-left border-b">Title</th>
+            <th className="py-2 px-4 text-left border-b">First Nssssame</th>
+            <th className="py-2 px-4 text-left border-b">Middle Name</th>
+            <th className="py-2 px-4 text-left border-b">Last Name</th>
+          </tr>
+        </thead>
+        <tbody>
+          {persons.map((p) => (
+            <tr key={p.businessEntityId} className="hover:bg-sky-800">
+              <td className="py-2 px-4 border-b">{p.businessEntityId}</td>
+              <td className="py-2 px-4 border-b">{p.title}</td>
+              <td className="py-2 px-4 border-b">{p.firstName}</td>
+              <td className="py-2 px-4 border-b">{p.middleName}</td>
+              <td className="py-2 px-4 border-b">{p.lastName}</td>
             </tr>
-          </thead>
-          <tbody>
-            {persons.map((p) => (
-              <tr key={p.businessEntityId} className="hover:bg-sky-800">
-                <td className="py-2 px-4 border-b">{p.businessEntityId}</td>
-                <td className="py-2 px-4 border-b">{p.title}</td>
-                <td className="py-2 px-4 border-b">{p.firstName}</td>
-                <td className="py-2 px-4 border-b">{p.middleName}</td>
-                <td className="py-2 px-4 border-b">{p.lastName}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
