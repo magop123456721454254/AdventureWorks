5901b12a5f855c4e989375ba17c99e188cdb1e39
magnussunesop <magnus.sunesson@omegapoint.se>
2025-11-07 14:49:25 +0100

    .

--- adventureworks-ui/next.config.ts
+++ adventureworks-ui/next.config.ts
@@ -5,3 +5,11 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
+// Source - https://stackoverflow.com/a
+// Posted by Joel Hernández, modified by community. See post 'Timeline' for change history
+// Retrieved 2025-11-07, License - CC BY-SA 4.0
+
+// next.config.js
+module.exports = {
+    trailingSlash: true,
+}
