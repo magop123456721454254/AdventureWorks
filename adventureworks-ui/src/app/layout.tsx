dc74f76f101548f73d96c511f22eef2c26defb75
magnussunesop <magnus.sunesson@omegapoint.se>
2025-11-07 14:52:00 +0100

    .

--- adventureworks-ui/src/app/layout.tsx
+++ adventureworks-ui/src/app/layout.tsx
@@ -1,6 +1,7 @@
 'use client';
 
-import { ApolloProvider } from '@apollo/client';
+import { ApolloClient, InMemoryCache, gql, ApolloProvider, useQuery } from '@apollo/client';
+export { ApolloClient, InMemoryCache, gql, ApolloProvider, useQuery };
 import { client } from './functions/client';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
