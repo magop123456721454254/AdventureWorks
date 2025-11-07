df0e1a83a6ba79173e92254d880800ea4c4ad371
magnussunesop <magnus.sunesson@omegapoint.se>
2025-11-07 11:18:31 +0100

    update page.tsx to use graphql call

--- adventureworks-ui/src/app/page.tsx
+++ adventureworks-ui/src/app/page.tsx
@@ -1,23 +1,27 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 'use client';
 import { useQuery } from '@apollo/client';
-import { GET_PERSONS } from './functions/queries/personQueries';
+import { GET_PERSONS_LIST_NO_PARAMETER } from './functions/queries/personQueries';
 import { Person } from '@/app/models/personModel';
 
 export default function PersonsPage() {
-  const { data, loading, error, fetchMore } = useQuery(GET_PERSONS, {
-    variables: { first: 5, after: null },
+  const { data, loading, error, fetchMore } = useQuery(GET_PERSONS_LIST_NO_PARAMETER, {
+    variables: { amount: 5, after: null }, // <-- pass your params here
   });
 
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error: {error.message}</p>;
 
+  if (loading) return <html><body><p>Loading...</p></body></html>;
+  if (error) return <html><body><p>Error: {error.message}</p></body></html>;
+
+  const nodes = data.personsList.edges.map((edge: any) => edge.node);
+  
   return (
     <html>
       <body>
         <ul>
-          {data.personsEndpoint.nodes.map((person: Person) => (
+          {nodes.map((person: Person) => (
             <li key={person.businessEntityId}>
-              {person.firstName} {person.lastName}
+              {person.title} {person.firstName} {person.personType}
             </li>
           ))}
         </ul>
